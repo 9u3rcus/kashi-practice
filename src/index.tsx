139e1faf85c48473dfc03edc76d95f55f2415633
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-10-23 00:08:23 +0900

    app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './router/App';
 import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 import { Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Route, BrowserRouter } from "react-router-dom";
@@ -19,5 +18,3 @@ if (container) {
 </BrowserRouter>
 );
 }
-
-reportWebVitals(sendToVercelAnalytics);
--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
 <div>
-
+<p>aiueo</p>
 </div>
   );
 }
